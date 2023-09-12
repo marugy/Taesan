d7aceae5d721b8ed7bcca4fca49e8a6516585351
배용현 <byh9811@naver.com>
2023-09-12 13:52:45 +0900

    BE/feat : 은행 서비스 초기 세팅

---
+++ Backend/mydata/src/main/java/com/taesan/mydata/MydataApplication.java
@@ -0,0 +1,13 @@
+package com.taesan.mydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydataApplication.class, args);
+	}
+
+}
