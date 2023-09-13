80a6f2dc5fd2609236ffe3b32e9985c05a209766
byh9811 <byh9811@naver.com>
2023-09-13 21:04:51 +0900

    BE/feat : ChallengeAPI 목업 완성

--- Backend/challenge/src/main/java/com/taesan/challenge/domain/api/ChallengeAPI.java
+++ Backend/challenge/src/main/java/com/taesan/challenge/domain/api/ChallengeAPI.java
@@ -1,10 +1,8 @@
 package com.taesan.challenge.domain.api;
 
 import com.taesan.challenge.domain.dto.request.ChallengeRegisterRequest;
-import com.taesan.challenge.domain.dto.response.ChallengeDetailResponse;
-import com.taesan.challenge.domain.dto.response.ChallengeInfo;
-import com.taesan.challenge.domain.dto.response.ChallengeListResponse;
-import com.taesan.challenge.domain.dto.response.ParticipantInfo;
+import com.taesan.challenge.domain.dto.request.ChallengeTransferRequest;
+import com.taesan.challenge.domain.dto.response.*;
 import com.taesan.challenge.global.api.ApiResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -65,10 +63,35 @@ public class ChallengeAPI {
         return OK(null);
     }
 
-    @GetMapping("/recruit")
-    public ApiResult<> getRecruitingChallenge() {
-        ArrayList<> challengeInfos = new ArrayList<>();
-        return OK(new ChallengeListResponse());
+    @GetMapping("/{id}/recruit")
+    public ApiResult<RecruitingChallengeResponse> getRecruitingChallenge(
+            @PathVariable("id") Long challengeId
+    ) {
+        ArrayList<ParticipantInfo> itemList = new ArrayList<>();
+        itemList.add(new ParticipantInfo());
+        RecruitingChallengeResponse build = RecruitingChallengeResponse.builder()
+                .participantList(itemList)
+                .build();
+        return OK(build);
+    }
+
+    @GetMapping("/{id}/progress")
+    public ApiResult<ProgressingChallengeResponse> getProgressingChallenge(
+            @PathVariable("id") Long challengeId
+    ) {
+        ArrayList<ParticipantInfo> itemList = new ArrayList<>();
+        itemList.add(new ParticipantInfo());
+        ProgressingChallengeResponse build = ProgressingChallengeResponse.builder()
+                .participantList(itemList)
+                .build();
+        return OK(build);
+    }
+
+    @PostMapping("/transfer")
+    public ApiResult<Void> startChallenge(
+            @RequestBody ChallengeTransferRequest challengeTransferRequest
+    ) {
+        return OK(null);
     }
 
 }
