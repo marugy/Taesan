01b8e2f41c8af72c3df549d52c390c64fa6159b5
배용현 <byh9811@naver.com>
2023-09-13 15:06:31 +0900

    BE/feat : IfbuyAPI 목업 코드 완성

---
+++ Backend/ifbuy/src/main/java/com/taesan/ifbuy/IfbuyApplication.java
@@ -0,0 +1,13 @@
+package com.taesan.ifbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfbuyApplication.class, args);
+	}
+
+}
