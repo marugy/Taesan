d455938e74d3ab8e3fc93a8f8cbec24dc4ff1f1d
PARKHEECHANG <shjc4623@gmail.com>
2023-09-18 17:59:57 +0900

    FE/feat/절약챌린지 틀

--- Frontend/src/components/ChallengeResult/ChallengeResultItem.tsx
+++ Frontend/src/components/ChallengeResult/ChallengeResultItem.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@material-tailwind/react';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -15,8 +16,8 @@ const ChallengeResultItem = () => {
       <div>목표소비금액 : ￦100,000원</div>
       <div>남은 금액 : ￦30,000원</div>
       {/* 이미 환전한 항목은 비활성화 */}
-      <button onClick={() => navigate('/challenge/result/detail')}>자세히 보기</button>
-      <button onClick={handleSave}>아낀 금액 환전</button>
+      <Button onClick={() => navigate('/challenge/result/detail')}>자세히 보기</Button>
+      <Button onClick={handleSave}>아낀 금액 환전</Button>
     </div>
   );
 };
