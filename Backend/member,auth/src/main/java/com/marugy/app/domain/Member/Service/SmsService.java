d6d9cd1f06182bedd69c461d7e920f38fc058861
marugy <gyulife7301@naver.com>
2023-09-19 18:02:43 +0900

    chore : config test connection

--- Backend/member,auth/src/main/java/com/marugy/app/domain/Member/Service/SmsService.java
+++ Backend/member,auth/src/main/java/com/marugy/app/domain/Member/Service/SmsService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -30,7 +31,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-@PropertySource("classpath:application-key.yml")
+//@PropertySource("classpath:application-key.yml")
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -38,7 +39,8 @@ public class SmsService {
 
     //휴대폰 인증 번호
     private final String smsConfirmNum = createSmsKey();
-    private final RedisUtil redisUtil;
+//    private final RedisUtil redisUtil;
+    private final Environment env;
 
     @Value("${naver-cloud-sms.accessKey}")
     private String accessKey;
@@ -51,6 +53,13 @@ public class SmsService {
 
     @Value("${naver-cloud-sms.senderPhone}")
     private String phone;
+
+    //spring core에서 값을 읽어
+    public SmsService(Environment env) {
+        this.env = env;
+        this.accessKey = env.getProperty("naver-cloud-sms.accessKey");
+
+    }
 
     public String getSignature(String time) throws NoSuchAlgorithmException, UnsupportedEncodingException, InvalidKeyException {
         String space = " ";
