66bd246960e410afb74c5a382764598c9dbe2e79
PARKHEECHANG <shjc4623@gmail.com>
2023-09-20 15:34:00 +0900

    FE/feat/핀코드

--- Frontend/src/components/Common/Pincode.tsx
+++ Frontend/src/components/Common/Pincode.tsx
@@ -1,55 +1,127 @@
+import { Button } from '@material-tailwind/react';
+import { IconButton } from '@material-tailwind/react';
+import BackspaceOutlinedIcon from '@mui/icons-material/BackspaceOutlined';
 import React, { useState } from 'react';
-import { ReactPaymentKeypad, ReactPaymentKeypadProps, ReturnValue } from 'react-payment-keypad';
 
-const Pincode = () => {
-  const [visible, setVisible] = useState(false);
-  const setting: ReactPaymentKeypadProps = {
-    // {Required} keypad 사용 여부 입니다.
-    isVisible: visible,
-    // {Optional} emptyPassword true 일 경우 패스워드를 2번 입력 하도록 변경
-    emptyPassword: false,
-    // {Optional}  패스워드 화면을 꽉 채웁니다
-    full: true,
-    // {Optional} 패스워드를 입력 하는 횟수를 정하는 props 입니다
-    count: 6,
-    // {Optional} 에러 메세지 props 입니다.
-    errorMessage: '패스워드가 일치 하지 않습니다.',
-    // {Optional} 메세지 커스텀을 위한 props 입니다.
-    messages: ['패스워드를 입력 해주세요.', '사용할 패스워드를 설정 해주세요', '다시 한번 입력해 주세요.'],
-    /**
-     *  숫자 키패드 정렬 props 입니다.
-     * - always 키패드 클릭 시 항상 패드를 재정렬 합니다.
-     * - fixed 숫자 순서 그대로 정렬 합니다.
-     * - once 키패드 입력 전 한번 랜덤으로 정렬 합니다.
-     * */
-    shuffle: 'fixed',
-    /**
-     *   전체삭제 버튼 커스텀을 위한 props 입니다.
-     *   @example
-     *    deleteAllIcon: <img src="/image/icon.png"/> or "전체삭제"
-     * */
-    deleteAllIcon: '전체 삭제버튼',
-    /**
-     * 삭제 버튼 커스텀을 위한 props 입니다.
-     *  @example
-     *    deleteAllIcon: <img src="/image/icon.png"/> or "삭제"
-     * */
-    deleteIcon: '삭제버튼',
-    // {Required} keypad close func
-    onClose: () => {
-      setVisible(false);
-    },
-    // {Required} keypad 입력 후 패스워드 결과 값이 나오는 func
-    onFinish: (password) => {
-      console.log(password);
-    },
-    // {Optional} emptyPassword true 일 경우 패스워드 결과 값이 return 되는 func
-    onPassConfirm: (password) => {
-      console.log(password);
-    },
+const MAX_LENGTH = 6;
+
+export const Pincode = () => {
+  // 저장된 PinCode 값
+
+  // 입력한 PinCode 값
+  const [stack, setStack] = useState<string[]>([]);
+  console.log(stack);
+
+  // 숫자 입력
+  const handlePushPin = (pin: string) => {
+    if (stack.length < MAX_LENGTH) {
+      setStack([...stack, pin]);
+    }
   };
 
-  return <ReactPaymentKeypad {...setting} opener={true} />;
-};
+  // 숫자 제거
+  const handlePopPin = () => {
+    setStack(stack.slice(0, -1));
+  };
 
-export default Pincode;
+  return (
+    <div className="flex-col">
+      <div className="text-main flex justify-center mt-10 text-2xl tb:text-3xl dt:text-4xl font-bold">암호 입력</div>
+      <div className="flex justify-center space-x-5 text-main text-[30px] my-16">
+        {Array.from({ length: MAX_LENGTH }).map((_, index) => {
+          if (index < stack.length) {
+            return <img src="/Pincode/check.png" alt="check" />;
+          }
+          return <img src="/Pincode/uncheck.png" alt="check" />;
+        })}
+      </div>
+      <div className="flex flex-col justify-center mb-16">
+        <div className="space-x-5 m-1 flex justify-end">
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('1')}
+          >
+            1
+          </Button>
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('2')}
+          >
+            2
+          </Button>
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('3')}
+          >
+            3
+          </Button>
+        </div>
+        <div className="space-x-5  m-1 flex justify-end">
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('4')}
+          >
+            4
+          </Button>
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('5')}
+          >
+            5
+          </Button>
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('6')}
+          >
+            6
+          </Button>
+        </div>
+        <div className="space-x-5 m-1  flex justify-end">
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('7')}
+          >
+            7
+          </Button>
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('8')}
+          >
+            8
+          </Button>
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('9')}
+          >
+            9
+          </Button>
+        </div>
+        <div className="space-x-5 m-1  flex justify-end">
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={() => handlePushPin('0')}
+          >
+            0
+          </Button>
+          <Button
+            variant="outlined"
+            className="text-5xl tb:text-6xl dt:text-7xl rounded-full text-main border-[#0067AC]"
+            onClick={handlePopPin}
+          >
+            X
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
