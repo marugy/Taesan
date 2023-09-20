237311adcb9df2fbc78a6b189f50b34807dee137
marugy <gyulife7301@naver.com>
2023-09-20 17:32:38 +0900

    refactor : memberInfoResponse

--- Backend/src/main/java/com/ts/taesan/domain/member/entity/Address.java
+++ Backend/src/main/java/com/ts/taesan/domain/member/entity/Address.java
@@ -1,9 +1,18 @@
 package com.ts.taesan.domain.member.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.DynamicInsert;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@DynamicInsert
 public class Address {
     private String address;
     @Column(name = "address_detail")
