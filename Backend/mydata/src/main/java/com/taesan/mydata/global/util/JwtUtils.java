e40a12cdaadf84e593ca8f80bcc126e9daa42be5
byh9811 <byh9811@naver.com>
2023-09-24 02:13:26 +0900

    BE/feat : Authorization (접근 토큰) 생성 로직 개발

--- Backend/mydata/src/main/java/com/taesan/mydata/global/util/JwtUtils.java
+++ Backend/mydata/src/main/java/com/taesan/mydata/global/util/JwtUtils.java
@@ -2,66 +2,104 @@ package com.taesan.mydata.global.util;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 @Component
 public class JwtUtils {
 
-    private static final String TOKEN_TYPE = "Bearer";
+    @Value("${jwt.access.key}")
+    private String keyPlain;
 
-    private final AuthTokenExtractor authTokenExtractor;
-    private final Key secretKey;
-    private final long validityInMilliseconds;
+    @Value("${jwt.access.valid-time}")
+    private long accessTokenValid;
 
-    public JwtUtils(final AuthTokenExtractor authTokenExtractor,
-                    @Value("${security.jwt.secret-key}") final String secretKey,
-                    @Value("${security.jwt.expire-length}") final long validityInMilliseconds) {
-        this.authTokenExtractor = authTokenExtractor;
-        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
-        this.validityInMilliseconds = validityInMilliseconds;
+    private SecretKey accessKey;
+
+    public SecretKey getSecretKey() {
+        if (accessKey == null) {
+            accessKey = createKey();
+        }
+
+        return accessKey;
     }
 
-    public String createAccessToken(final Long id) {
-        final Date now = new Date();
-        final Date validity = new Date(now.getTime() + validityInMilliseconds);
+    // 키 생성
+    protected SecretKey createKey() {
+        return Keys.hmacShaKeyFor(Base64.getEncoder().encodeToString(keyPlain.getBytes()).getBytes());
+    }
 
+    // JWT 토큰 생성
+    public String createToken(String userCi) {
         return Jwts.builder()
-                .setSubject(id.toString())
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .setHeader(createHeader())
+                .setClaims(createClaims(userCi))
+                .signWith(getSecretKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public boolean validateToken(final String authorizationHeader) {
-        final String token = authTokenExtractor.extractToken(authorizationHeader, TOKEN_TYPE);
+    private Map<String, Object> createHeader() {
+        Map<String, Object> header = new HashMap<>();
+
+        header.put("typ","JWT");
+        header.put("alg","HS256");
+
+        return header;
+    }
+
+    private Claims createClaims(String userCi) {
+        Claims claims = Jwts.claims();
+
+        claims.setIssuer("Bank");
+        claims.setAudience("Taesan");
+        claims.setId(UUID.randomUUID().toString());
+        claims.setExpiration(new Date(new Date().getTime() + accessTokenValid));
+        claims.put("scope", setScope());
+        claims.put("user-ci", userCi);
+
+        return claims;
+    }
+
+    private String setScope() {
+        List<String> scopeList = getAllowedScopes();
+        StringBuilder sb = new StringBuilder();
+
+        for (String scope : scopeList) {
+            sb.append(scope).append(' ');
+        }
+
+        return sb.substring(0, sb.length() - 1);
+    }
+
+    private List<String> getAllowedScopes() {
+        List<String> scopeList = new ArrayList<>();
+
+        scopeList.add("bank.list");
+        scopeList.add("bank.deposit");
+        scopeList.add("card.list");
+        scopeList.add("card.card");
+
+        return scopeList;
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
+    }
+
+    public boolean validateToken(String jwtToken) {
         try {
-            final Jws<Claims> claims = getClaimsJws(token);
-            return !claims
-                    .getBody()
-                    .getExpiration()
-                    .before(new Date());
-        } catch (JwtException | IllegalArgumentException e) {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(accessKey).build()
+                    .parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
             return false;
         }
-    }
-
-    private Jws<Claims> getClaimsJws(final String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
-                .build()
-                .parseClaimsJws(token);
-    }
-
-    public String getPayload(final String authorizationHeader) {
-        final String token = authTokenExtractor.extractToken(authorizationHeader, TOKEN_TYPE);
-        return getClaimsJws(token)
-                .getBody()
-                .getSubject();
     }
 }
