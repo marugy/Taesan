027d4ad9030f5d64f5761be9b58e8a19a2703f7d
marugy <gyulife7301@naver.com>
2023-09-25 20:55:04 +0900

    fix:swagger

--- Backend/src/main/java/com/ts/taesan/global/config/SwaggerConfig.java
+++ Backend/src/main/java/com/ts/taesan/global/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.ts.taesan.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,46 +16,39 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Arrays;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ts.taesan.domain"))
-                .paths(PathSelectors.any())
-                .build()
-                .host("https://j9c211.p.ssafy.io/api")
-//                .securitySchemes(Arrays.asList(apiKey()))
-//                .securityContexts(Arrays.asList(securityContext()))
-                .apiInfo(apiInfo());
-    }
-
-    // 추가
-//    private SecurityContext securityContext() {
-//        return SecurityContext.builder()
-//                .securityReferences(defaultAuth())
-//                .build();
+//@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    //    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .useDefaultResponseMessages(false)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.ts.taesan.domain"))
+//                .paths(PathSelectors.any())
+//                .build()
+//                .host("https://j9c211.p.ssafy.io/api")
+////                .securitySchemes(Arrays.asList(apiKey()))
+////                .securityContexts(Arrays.asList(securityContext()))
+//                .apiInfo(apiInfo());
 //    }
 //
-//    private List<SecurityReference> defaultAuth() {
-//        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-//        authorizationScopes[0] = authorizationScope;
-//        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
-//    }
-//
-//    // 추가
-//    private ApiKey apiKey() {
-//        return new ApiKey("Authorization", "Authorization", "header");
-//    }
-
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("TAESAN REST API")
                 .description("금융 절약 플랫폼 TAESAN의 API 명세서 입니다")
                 .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public Docket commonApi() {
+        return new Docket(DocumentationType.OAS_30) // 3.0 문서버전으로 세팅
+                .groupName("example")
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ts.taesan.domain"))
+                .paths(PathSelectors.ant("/**"))
                 .build();
     }
 }
