e83a85ac509109b926921f573f24da9d5c54e3fc
이지헌 <jiheon_114@naver.com>
2023-10-02 00:31:04 +0900

    FE/feat: 카테고리에 따른 아이콘 띄우기 위해 아이콘 세팅, 마이페이지 중 계좌변경 페이지에서 계좌 조회하는 기능 구현

--- Frontend/src/pages/MyPage/ChangeAccountPage.tsx
+++ Frontend/src/pages/MyPage/ChangeAccountPage.tsx
@@ -6,8 +6,36 @@ import { List, ListItem, ListItemPrefix, Avatar, Card, Typography } from '@mater
 import { ACCOUNT_DATA } from 'constants/DUMMY';
 import ArrowBack from 'components/Common/ArrowBack';
 import BottomNav from 'components/Common/BottomNav';
+import { useQuery } from 'react-query';
+import { useUserStore } from 'store/UserStore';
+import axios from 'axios';
 
 const ChangeAccountPage = () => {
+  const { accessToken,refreshToken} = useUserStore();
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [changeAccount,setChangeAccount] = useState('');
+
+  const handleItemClick = (index: any, accountNum: string) => {
+    setSelectedItem(index);
+    // console.log(accountNum);
+  };
+
+  // 1.계좌 목록 불러오는 함수
+  const getAccountList = async () => {
+    const { data: accountList } = await axios.get('https://j9c211.p.ssafy.io/api/asset-management/assets/account/list', {
+        headers: {
+          'ACCESS-TOKEN': accessToken,
+          'REFRESH-TOKEN': refreshToken,
+        },
+      
+    });
+    console.log(accountList);
+    return accountList;
+  };
+  const useAccountQuery = useQuery('getAccountList', getAccountList);
+
+  // 2. 내 자산 조회하는 함수
+  
   const [nextButton, setNextButton] = useState(false);
   return (
     <div className='h-scren flex flex-col'>
@@ -17,7 +45,8 @@ const ChangeAccountPage = () => {
         현재 등록 계좌
       </Typography>
       <Card className="w-96">
-        <ListItem>
+          <List>
+          <ListItem>
           <ListItemPrefix>
             <Avatar
               variant="circular"
@@ -37,17 +66,21 @@ const ChangeAccountPage = () => {
             </div>
 
             <div className="flex items-center">
-              <Typography variant="paragraph" color="blue" className="font-normal font-semibold">
+              <Typography variant="paragraph" color="blue" className="font-normal ">
                 {ACCOUNT_DATA[0].balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
               </Typography>
             </div>
           </div>
         </ListItem>
-      </Card>
+          </List>
+        </Card>
 
       <hr className="my-5 border-gray-400 border-1 w-full" />
       {/* 얘 나중에 수정해야함 (지헌 할 것) */}
-      <AccountRegister accountList={[]} setAccount=""/>
+
+      {useAccountQuery.data && useAccountQuery.data.response.accountList ? (
+        <AccountRegister setAccount={setChangeAccount} accountList={useAccountQuery.data.response.accountList} />
+      ) : null}
       <BottomNav/>
     </div>
     </div>
