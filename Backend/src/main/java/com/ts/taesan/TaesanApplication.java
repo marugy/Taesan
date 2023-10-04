69201ea10fac4649da6e3c9357f59fe28ef87f98
marugy <gyulife7301@naver.com>
2023-10-04 16:13:00 +0900

    BE/fix : challenge endTime

--- Backend/src/main/java/com/ts/taesan/TaesanApplication.java
+++ Backend/src/main/java/com/ts/taesan/TaesanApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
@@ -12,6 +15,11 @@ public class TaesanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaesanApplication.class, args);
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
