b715afbb7c1d88f85761d1e60268f247b2ab103e
김하영 <bnk159hair@naver.com>
2023-10-06 00:40:35 +0900

    ALL/feat : 산출물 추가

--- exec/dump.sql
+++ exec/dump.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `challenge`
 --
 
+
+
 DROP TABLE IF EXISTS `challenge`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
